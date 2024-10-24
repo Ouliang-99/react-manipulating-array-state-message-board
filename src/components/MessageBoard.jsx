5d845c17c79b30cb6a040ea1b0d5de2115f38ff0
OLEANG <wannachok.job@gmail.com>
2024-10-24 13:00:07 +0700

    Oleang/manipulating-array-state-message-board

--- src/components/MessageBoard.jsx
+++ src/components/MessageBoard.jsx
@@ -1,23 +1,53 @@
+import { useState } from "react";
+
 function MessageBoard() {
+  const [message, setMessage] = useState([]);
+  const [displayMessage, setDisplayMessage] = useState("");
+
+  const addMessage = (event) => {
+    event.preventDefault();
+    setMessage([...message, displayMessage]);
+  };
+
+  const deleteMessage = (index) => {
+    setMessage(message.filter((_, i) => i !== index));
+  };
+  
+
   return (
     <div className="app-wrapper">
       <h1 class="app-title">Message board</h1>
-      <div class="message-input-container">
-        <label>
-          <input
-            id="message-text"
-            name="message-text"
-            type="text"
-            placeholder="Enter message here"
-          />
-        </label>
-        <button className="submit-message-button">Submit</button>
-      </div>
-      <div class="board">
-        <div className="message">
-          <h1>Hello all ! This is first message.</h1>
-          <button className="delete-button">x</button>
+      <form onSubmit={addMessage}>
+        <div class="message-input-container">
+          <label>
+            <input
+              id="message-text"
+              name="message-text"
+              type="text"
+              placeholder="Enter message here"
+              value={displayMessage}
+              onChange={(event) => {
+                setDisplayMessage(event.target.value);
+              }}
+            />
+          </label>
+          <button className="submit-message-button">Submit</button>
         </div>
+      </form>
+      <div class="board">
+        {message.map((message, index) => {
+          return (
+            <div key={index} className="message">
+              <h1>{message}</h1>
+              <button
+                onClick={() => deleteMessage(index)}
+                className="delete-button"
+              >
+                x
+              </button>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
